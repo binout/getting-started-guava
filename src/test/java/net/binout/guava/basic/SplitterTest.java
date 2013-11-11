1c8dc1616e351cf88ed8682a041156da46b8b0d6
unknown <b.prioux@WRDSCOM04.eu.lectra.com>
2013-11-11 19:35:02 +0100

    add samples on splitter

---
+++ src/test/java/net/binout/guava/basic/SplitterTest.java
@@ -0,0 +1,26 @@
+package net.binout.guava.basic;
+
+import com.google.common.base.Splitter;
+import org.assertj.core.data.MapEntry;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SplitterTest {
+
+    @Test
+    public void splitter_with_trim() {
+        List<String> results = Splitter.on(",").trimResults().splitToList("PSG, OM, FCGB");
+        assertThat(results).contains("PSG", "OM", "FCGB");
+    }
+
+    @Test
+    public void map_splitter() {
+        Map<String,String> resultMap = Splitter.on(".").withKeyValueSeparator("#").split("Paris#PSG.Marseille#OM.Bordeaux#FCGB");
+        assertThat(resultMap).contains(MapEntry.entry("Paris", "PSG")).contains(MapEntry.entry("Marseille", "OM")).contains(MapEntry.entry("Bordeaux", "FCGB"));
+    }
+
+}
